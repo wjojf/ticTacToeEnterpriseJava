8246574127fc0b8f4669b4b54927bae033611c40
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-09-25 22:31:47 +0200

    cleanup

--- src/main/java/game/domain/repository/IPlayerStatsRepository.java
+++ src/main/java/game/domain/repository/IPlayerStatsRepository.java
@@ -2,11 +2,9 @@ package game.domain.repository;
 
 import game.domain.models.PlayerStats;
 
-import java.security.InvalidKeyException;
-
 public interface IPlayerStatsRepository {
-    public PlayerStats getPlayerStatsByUserID(Integer userId) throws Exception, InvalidKeyException;
-    public void updatePlayerStatsByUserId(Integer userId, PlayerStats playerStats) throws Exception;
-    public void createPlayerStatsByUserId(Integer userId, PlayerStats playerStats) throws Exception;
+    PlayerStats getPlayerStatsByUserID(Integer userId) throws Exception;
+    void updatePlayerStatsByUserId(Integer userId, PlayerStats playerStats) throws Exception;
+    void createPlayerStatsByUserId(Integer userId, PlayerStats playerStats) throws Exception;
 
 }
